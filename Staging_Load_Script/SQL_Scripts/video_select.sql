2b2de698552adafb18759f23980e91f286e1a437
Kyle Bales <kbalesdev@gmail.com>
2022-01-16 23:04:31 -0600

    Using SQL Alchemy to read from source table into a DataFrame and bulk insert the data into a new table

---
+++ Staging_Load_Script/SQL_Scripts/video_select.sql
@@ -0,0 +1,13 @@
+SELECT VideoId
+	   ,ApiDetailUrl
+	   ,Deck
+	   ,EmbedPlayer
+	   ,[Guid]
+	   ,LengthSeconds
+	   ,[Name]
+	   ,Premium
+	   ,PublishDate
+	   ,SiteDetailUrl
+	   ,VideoType
+	   ,HdUrl
+FROM Videos
